0ae27ad317bec8d2df9718c8391e137e1ed70ee0
shyqulreyad <shyqulislam@gmail.com>
2022-10-21 12:51:08 +0600

    Read and Delete

--- database.php
+++ database.php
@@ -14,10 +14,61 @@
 
 
     class query extends database{
+        // get data from database
         public function getData($table_name){
             $sql = "SELECT * FROM $table_name";
             $result = $this->connect()->query($sql);
-            print_r($result);
+            if($result->num_rows > 0){
+                $data = array();
+                while($row = $result->fetch_assoc()){
+                    $data[] = $row;
+                }
+                return $data;
+            }else{
+                return false;
+            }
+        }
+    
+        // Insert data into database
+        public function insertData($table_name, $fields){
+            $sql = "";
+            $sql .= "INSERT INTO $table_name (".implode(",", array_keys($fields)).") VALUES ('".implode("','", array_values($fields))."')";
+            $query = $this->connect()->query($sql);
+            if($query){
+                return true;
+            }
+        }
+
+        // Update data into database
+        public function updateData($table_name, $fields, $where){
+            $sql = "";
+            $condition = "";
+            foreach($where as $key => $value){
+                $condition .= $key . "='" . $value . "' AND ";
+            }
+            $condition = substr($condition, 0, -5);
+            foreach($fields as $key => $value){
+                $sql .= $key . "='".$value."', ";
+            }
+            $sql = substr($sql, 0, -2);
+            $sql = "UPDATE $table_name SET $sql WHERE $condition";
+            if(mysqli_query($this->connect(), $sql)){
+                return true;
+            }
+        }
+
+        // Delete data from database
+        public function deleteData($table_name, $where){
+            $sql = "";
+            $condition = "";
+            foreach($where as $key => $value){
+                $condition .= $key . "='" . $value . "' AND ";
+            }
+            $condition = substr($condition, 0, -5);
+            $sql = "DELETE FROM $table_name WHERE $condition";
+            if(mysqli_query($this->connect(), $sql)){
+                return true;
+            }
         }
     }
 
